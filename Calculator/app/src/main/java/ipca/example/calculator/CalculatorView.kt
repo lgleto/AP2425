8593ac8f84ad9acceca303bc794f56303fc2b57a
Lourenço <leto.lg@gmail.com>
2024-10-31 16:24:31 +0000

    operations

--- Calculator/app/src/main/java/ipca/example/calculator/CalculatorView.kt
+++ Calculator/app/src/main/java/ipca/example/calculator/CalculatorView.kt
@@ -30,6 +30,59 @@ import ipca.example.calculator.ui.theme.PurpleGrey40
 fun CalculatorView(modifier: Modifier = Modifier) {
 
     var display by remember { mutableStateOf("0") }
+    var operation by remember { mutableStateOf("") }
+    var operator by remember { mutableStateOf(0.0) }
+    var userIsTyping by remember { mutableStateOf(false) }
+
+    fun getDisplay() : Double{
+        return display.toDouble()
+    }
+
+    fun setDisplay(value : Double){
+        display = value.toString()
+    }
+
+    val onClickOperation : (String) -> Unit = { op ->
+        userIsTyping = false
+
+        when(operation){
+            "+"->{
+                setDisplay(operator + getDisplay())
+            }
+            "-"->{
+                setDisplay(operator - getDisplay())
+            }
+            "/"->{
+                setDisplay(operator / getDisplay())
+            }
+            "*"->{
+                setDisplay(operator * getDisplay())
+            }
+        }
+        operator = getDisplay()
+        operation = op
+    }
+
+    val onClickNumber : (String) -> Unit = { number ->
+        if (userIsTyping) {
+            if (display == "0" && number != ".") {
+                display = number
+            }else if(number == "." ) {
+                if(!display.contains(".")) {
+                    display += number
+                }
+            }else {
+                display += number
+            }
+        }else{
+            display = number
+        }
+        userIsTyping = true
+    }
+
+
+
+
 
     Column(modifier = modifier
         .fillMaxSize()) {
@@ -41,21 +94,79 @@ fun CalculatorView(modifier: Modifier = Modifier) {
         Row{
             CalcButton(
                 modifier = Modifier.weight(1f),
-                label = "1",
-                onClick = {})
+                label = "9",
+                onClick = onClickNumber)
             CalcButton(
                 modifier = Modifier.weight(1f),
-                label = "2",
-                onClick = {})
+                label = "8",
+                onClick = onClickNumber)
             CalcButton(
                 modifier = Modifier.weight(1f),
-                label = "3",
-                onClick = {})
+                label = "7",
+                onClick = onClickNumber)
             CalcButton(
                 modifier = Modifier.weight(1f),
                 label = "+",
                 isOperation = true,
-                onClick = {})
+                onClick = onClickOperation)
+        }
+        Row{
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "6",
+                onClick = onClickNumber)
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "5",
+                onClick = onClickNumber)
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "4",
+                onClick = onClickNumber)
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "-",
+                isOperation = true,
+                onClick = onClickOperation)
+        }
+        Row{
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "1",
+                onClick = onClickNumber)
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "2",
+                onClick = onClickNumber)
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "3",
+                onClick = onClickNumber)
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "*",
+                isOperation = true,
+                onClick = onClickOperation)
+        }
+        Row{
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "0",
+                onClick = onClickNumber)
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = ".",
+                onClick = onClickNumber)
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "=",
+                isOperation = true,
+                onClick = onClickOperation)
+            CalcButton(
+                modifier = Modifier.weight(1f),
+                label = "/",
+                isOperation = true,
+                onClick = onClickOperation)
         }
 
     }
